1df74e332160f6aa1af44a0600f79bbf3f926800
Seelam Siddhartha Reddy <101920592+siddhuseelam@users.noreply.github.com>
2025-06-12 09:13:01 +0530

    4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -14,8 +14,16 @@ const totalLikes = blogs => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 }
 
+const favoriteBlog = blogs => {
+    if (blogs.length === 0) {
+        return null;
+    }
+    return blogs.reduce((max, blog) => (blog.likes > max.likes ? blog : max), blogs[0]);
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
 
