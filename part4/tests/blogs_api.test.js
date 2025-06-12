babb2d19841e043fa4345c0fc3f4c1dd92d3d6ba
Seelam Siddhartha Reddy <101920592+siddhuseelam@users.noreply.github.com>
2025-06-12 20:37:22 +0530

    4.10 completed

--- part4/tests/blogs_api.test.js
+++ part4/tests/blogs_api.test.js
@@ -1,33 +1,19 @@
 const { test, beforeEach, after } = require('node:test')
+const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
+const helper = require('./test_helper')
 
 
 
-const initialBlogs = [
-  {
-    title: 'First Blog',
-    author: 'John Doe',
-    url: 'https://example.com/first-blog',
-    likes: 5
-  },
-  {
-    title: 'Second Blog',
-    author: 'Jane Doe',
-    url: 'https://example.com/second-blog',
-    likes: 10
-  }
-]
+const initialBlogs = helper.initialBlogs
 
 beforeEach(async () => {
   await Blog.deleteMany({})
-  let blogObject = new Blog(initialBlogs[0])
-  await blogObject.save()
-  blogObject = new Blog(initialBlogs[1])
-  await blogObject.save()
+  await Blog.insertMany(initialBlogs)
 })
 
 test('blogs are returned as json', async () => {
@@ -36,6 +22,36 @@ test('blogs are returned as json', async () => {
     .expect(200)
     .expect('Content-Type', /application\/json/)
 })
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+  assert.strictEqual(response.body.length, helper.initialBlogs.length)
+})
+
+test('blog identifier is named id', async () => {
+  const response = await api.get('/api/blogs')
+  response.body.forEach(blog => {
+    assert.ok(blog.id, 'Blog does not have an id field')
+  })
+})
+
+test('sending a post request to create a new blog', async()=> {
+  const newBlog = {
+    title: 'New Blog',
+    author: 'New Author',
+    url: 'https://example.com/new-blog',
+    likes: 15
+  }
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  assert.deepStrictEqual(response.body.length, initialBlogs.length + 1)
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
