01fe7afc5889f35f58f817eee7888f77ec532e96
Seelam Siddhartha Reddy <101920592+siddhuseelam@users.noreply.github.com>
2025-06-13 16:41:43 +0530

    exercise 5.4 completed

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -2,7 +2,52 @@ import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import Login from './components/Login'
+import CreateBlog from './components/CreateBlog'
+
+
+
+
+const errorMessage = (message) => {
+  const errorStyle = {
+    color: 'red',
+    background: 'lightgrey',
+    fontSize: 20,
+    borderStyle: 'solid',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10
+  }
+  return (
+    <div style={errorStyle}>
+      {message}
+    </div>
+  )
+}
+
+const successMessage = (message) => {
+  const successStyle = {
+    color: 'green',
+    background: 'lightgrey',
+    fontSize: 20,
+    borderStyle: 'solid',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10
+  }
+  return (
+    <div style={successStyle}>
+      {message}
+    </div>
+  )
+}
+
+
+
+
 const App = () => {
+
+  const [error, setError] = useState(null)
+  const [success, setSuccess] = useState(null)
   const [blogs, setBlogs] = useState([])
 
   useEffect(() => {
@@ -33,15 +78,47 @@ const App = () => {
         window.localStorage.setItem('loggedBlogAppUser', JSON.stringify(response))
         setUser(response)
         blogService.setToken(response.token)
+        setSuccess('sucessfully logged in ')
+        setTimeout(() => {
+        setSuccess(null)
+      }, 1000)
       }
     } catch (error) {
       console.error('Login failed:', error)
+      setError('Wrong credentials')
+      setTimeout(() => {
+        setError(null)
+      }, 1000)
+    }
+  }
+
+  const createBlog = async (blogObject) => {
+    try{
+
+      const response  = await blogService.create(blogObject)
+      setBlogs(blogs.concat(response))
+      console.log('Blog created:', response)
+      
+      setSuccess('blog created sucessfully')
+      setTimeout(()=>{
+        setSuccess(null)
+      }, 1000
+    )
+
+    }
+    catch(exception){
+      console.error('Error creating blog:', exception)
+      setError('Failed to create a blog')
+      setTimeout(() => {
+        setError(null)
+      }, 1000)
     }
   }
 
   return (
     <>
-
+    {error && errorMessage(error)}
+    {success && successMessage(success)}
     
     {user ? <h1>Blogs</h1> : <h1>Please log in to see blogs</h1>}
     {!user && 
@@ -49,6 +126,10 @@ const App = () => {
     {user && <p>{user.name} logged in <button onClick={()=>{window.localStorage.removeItem('loggedBlogAppUser'); setUser(null); blogService.setToken(null);}}>Logout</button></p>}
 
     {user &&
+    <>
+
+    <h3>Create Blog</h3>
+    <CreateBlog createBlog={createBlog} />
     <div>
       
       {blogs.map(blog =>
@@ -56,6 +137,7 @@ const App = () => {
       )}
 
     </div>
+      </>
     }
       </>
   )
--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -9,6 +9,9 @@ const getAll = () => {
   })
 }
 
+
+
+
 let token = null
 
 const setToken = (newToken) => {
@@ -22,4 +25,14 @@ const login = async (credentials) => {
   return response.data
 }
 
-export default { getAll, login, setToken }
+
+const create = async (blogObject) => {
+  const config = {
+    headers: { Authorization: token }
+  }
+
+  const response = await axios.post(`${baseUrl}/blogs`, blogObject, config)
+  return response.data
+}
+
+export default { getAll, login, setToken, create }
